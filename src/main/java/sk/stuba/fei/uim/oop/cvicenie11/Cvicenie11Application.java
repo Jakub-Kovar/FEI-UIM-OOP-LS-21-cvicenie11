ba23e8e8055619315d68c0855a7889966b23fd42
Jakub Kovář <jakub.kovar@stuba.sk>
2021-05-03 13:41:49 +0200

    initial commit

---
+++ src/main/java/sk/stuba/fei/uim/oop/cvicenie11/Cvicenie11Application.java
@@ -0,0 +1,13 @@
+package sk.stuba.fei.uim.oop.cvicenie11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cvicenie11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvicenie11Application.class, args);
+    }
+
+}
