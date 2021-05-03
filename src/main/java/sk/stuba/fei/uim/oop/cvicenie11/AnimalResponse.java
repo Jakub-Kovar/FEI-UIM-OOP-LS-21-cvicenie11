d6a95ecf066550fdcf5cd0d8f4827cd614e40e40
Jakub Kovář <jakub.kovar@stuba.sk>
2021-05-03 14:31:02 +0200

    test DB entries

--- src/main/java/sk/stuba/fei/uim/oop/cvicenie11/AnimalResponse.java
+++ src/main/java/sk/stuba/fei/uim/oop/cvicenie11/AnimalResponse.java
@@ -4,9 +4,13 @@ import lombok.Getter;
 
 @Getter
 public class AnimalResponse {
+
+    private Long id;
+
     private String species;
 
     public AnimalResponse(Animal a) {
+        this.id = a.getId();
         this.species = a.getSpecies();
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/cvicenie11/AnimalService.java
+++ src/main/java/sk/stuba/fei/uim/oop/cvicenie11/AnimalService.java
@@ -9,8 +9,21 @@ import java.util.List;
 @Service
 public class AnimalService implements IAnimalService {
 
-    @Autowired
+
     private AnimalRepository repository;
+
+    @Autowired
+    public AnimalService(AnimalRepository repository) {
+        this.repository = repository;
+        Animal a1 = new Animal();
+        a1.setSpecies("Dog");
+        this.repository.save(a1);
+        Animal a2 = new Animal();
+        a2.setSpecies("Cat");
+        System.out.println(a2.getId());
+        a2 = this.repository.save(a2);
+        System.out.println(a2.getId());
+    }
 
     @Override
     public List<Animal> getAll() {
